b0092bed190fc4338d3ea4a5604c06b11f851ea6
Ollie Bettany <oliver.bettany@gmail.com>
2010-10-29 16:52:38 +0100

    Prem helped with a bit of optimisation

--- jquery_dd_reporter.0.1.js
+++ jquery_dd_reporter.0.1.js
@@ -1,24 +1,37 @@
-$(document).ready(function() {
-  // Handler for .ready() called.
-  $(window).resize(function(){
-    $('#dim_w_width').val($(window).width());   // returns width of browser viewport
-    $('#dim_w_height').val($(window).height());   // returns width of browser viewport
-  
-    $('#dim_d_width').val($(document).width()); // returns width of HTML document
-    $('#dim_d_height').val($(document).height()); // returns width of HTML document
-  });
-  
-  var link_title = 'Hide values';
-  $('#dim_checker a').click(function() {
-    if (link_title === 'Show values') {
-      link_title = 'Hide values';
-    } else {
-      link_title = 'Show values';
-    }  
-    $('#dim_checker a').html(link_title);
+/*----------------------------------
+ jQuery Document Dimensions Reporter
+ Version: 0.1
+ Developer: Oliver Bettany
+ Date: October 2010
+-----------------------------------*/
+
+  function twist_dd_reporter() {
+    var window = this,
+        $ = window.jQuery,
+        win = $(window),
+        doc = $(document),
+        dim_ww = $('#dim_w_width'),
+        dim_wh = $('#dim_w_height'),
+        dim_dw = $('#dim_d_width'),
+        dim_dh = $('#dim_d_height');
     
-    $('#dim_info').toggle(function() {
-      // Animation complete.
+    $(window).resize(function(){
+      dim_ww.val(win.width());   // returns width of browser viewport
+      dim_wh.val(win.height());   // returns width of browser viewport
+
+      dim_dw.val(doc.width()); // returns width of HTML document
+      dim_dh.val(doc.height()); // returns width of HTML document
     });
-  });
-});
+
+    var link_title = 'Hide values';
+    $('#dim_checker a').click(function() {
+      if (link_title === 'Show values') {
+        link_title = 'Hide values';
+      } else {
+        link_title = 'Show values';
+      }  
+      $('#dim_checker a').html(link_title);
+
+      $('#dim_info').toggle(function() {});
+    });
+  }
